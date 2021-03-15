94427580cba7405ca27705280cc7c06b2930c632
Dennis Nieuwenhuis <dennis.nieuwenhuis@iquality.nl>
2021-03-15 21:22:45 +0100

    Simplified navigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,3 @@
-const UglifyJS = require("uglify-js");
-
 module.exports = (eleventyConfig) => {
     // Add some utility filters
     eleventyConfig.addFilter("dateDisplay", require("./src/filters/date.js"));
@@ -7,16 +5,6 @@ module.exports = (eleventyConfig) => {
     eleventyConfig.addWatchTarget("./src/scss/");
 
     eleventyConfig.addPassthroughCopy("./src/fonts")
-
-    // Minify JS
-    eleventyConfig.addFilter("jsmin", function (code) {
-        let minified = UglifyJS.minify(code);
-        if (minified.error) {
-            console.log("UglifyJS error: ", minified.error);
-            return code;
-        }
-        return minified.code;
-    });
 
     return {
         dir: {
